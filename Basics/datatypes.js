eca154d110fd0ae8b6c40edb57385af0b001c4c5
TashonBraganca <tashon.braganca@gmail.com>
2026-02-06 00:27:09 +0530

    variables

---
+++ Basics/datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer vreison
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3+3);
+console.log("Hi there");
+
+let name="hitesh" // string datatype 
+let age=10 //int datatype 
+let isIn = false // boolean
+
+// string =>""
+//boolean => true/false
+//null => standalone value  (empty value)
+//udefined => value that hasnt been defined yet 
+//symbol =>  unique values 
+
+
+let name1 ="Tashon";
+console.log(name1);
+console.log(typeof name);
+
+
---
+++ Basics/variabls.js
@@ -0,0 +1,7 @@
+const accountId = 144553
+let accountEmail="tahson@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+accountId=2
+console.log(accountId);
